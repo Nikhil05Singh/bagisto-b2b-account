6014b07b3f3e38162d2eb998a0ad72c17b19b30b
NIKHIL SINGH <singh0507nikhil@gmail.com>
2026-03-01 00:42:43 +0530

    Added routes and store methods

--- src/Http/Controllers/Shop/RoleController.php
+++ src/Http/Controllers/Shop/RoleController.php
@@ -24,4 +24,25 @@ class RoleController extends Controller
     {
         return view('b2baccount::shop.roles.create');
     }
+
+    // This method catches the form data and saves the role
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'            => 'required|string|max:255',
+            'description'     => 'required|string',
+            'permission_type' => 'required|in:all,custom',
+        ]);
+
+        $data = $request->only(['name', 'description', 'permission_type']);
+        $data['permissions'] = $request->has('permissions') ? $request->permissions : [];
+        
+        // Link the role to the main B2B customer creating it
+        $data['customer_id'] = auth()->guard('customer')->user()->id;
+
+        $this->companyRoleRepository->create($data);
+
+        session()->flash('success', 'Role successfully created!');
+        return redirect()->route('b2baccount.roles.index');
+    }
 }
--- src/Http/Controllers/Shop/UserController.php
+++ src/Http/Controllers/Shop/UserController.php
@@ -24,6 +24,39 @@ class UserController extends Controller
 
     public function create()
     {
-        return view('b2baccount::shop.users.create');
+        $customer = auth()->guard('customer')->user();
+        $roles = $this->companyRoleRepository->findWhere(['customer_id' => $customer->id]);
+        
+        return view('b2baccount::shop.users.create', compact('roles'));
+    }
+
+    // This method catches the form data and saves the user
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name'      => 'required',
+            'last_name'       => 'required',
+            'email'           => 'required|email|unique:customers,email',
+            'phone'           => 'required',
+            'gender'          => 'required|in:Male,Female,Other',
+            'company_role_id' => 'required',
+        ]);
+
+        $data = $request->only(['first_name', 'last_name', 'email', 'phone', 'gender', 'company_role_id']);
+        
+        // Auto-generate a secure background password
+        $data['password'] = bcrypt(rand(100000, 99999999));
+        $data['status'] = $request->has('status') ? 1 : 0;
+        $data['is_suspended'] = 0;
+        $data['type'] = 'user';
+        
+        // Assign to default customer group (usually ID 2)
+        $data['customer_group_id'] = 2; 
+        $data['channel_id'] = core()->getCurrentChannel()->id;
+
+        $this->customerRepository->create($data);
+
+        session()->flash('success', 'User successfully created!');
+        return redirect()->route('b2baccount.users.index');
     }
 }
